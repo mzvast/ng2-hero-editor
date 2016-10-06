38a68e9775deed5b8285f88db14a05ab7c0c3576
mzvast <mzvast@gmail.com>
2016-10-06 21:05:49 +0800

    fully done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
+import './rxjs-extensions';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 import {ManageRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroService }          from './hero.service';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     ManageRoutingModule
   ],
   providers: [HeroService],
